789993170de1575ed276daca7dc51f89e2bbb39f
abli025 <152368927+abli025@users.noreply.github.com>
2024-03-11 22:32:36 +0530

    Time: 3 ms (50.27%), Space: 7.4 MB (78.20%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -4,15 +4,19 @@ public:
         int hash[26];
         std::string ans;
         memset(hash, 0, sizeof(hash));
+        // step 1 : push string s occurance to hash
         for (auto c : s) {
             hash[c-'a']++;
         }
+        // step 2 : for every order string char
+        //          reduce the count of string s and add to result
         for (auto c : order) {
             while (hash[c-'a'] > 0) {
                 ans.push_back(c);
                 hash[c-'a']--;
             }
         }
+        // step 3: loop thru rest of the char with occurance
         for (int i = 0; i < 26; i++) {
             while (hash[i] > 0) {
                 ans.push_back(i +'a');

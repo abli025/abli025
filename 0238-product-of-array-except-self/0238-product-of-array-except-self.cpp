7a494bddfd8f8b277f89633df6564acc210183fe
abli025 <152368927+abli025@users.noreply.github.com>
2024-03-15 22:20:24 +0530

    Time: 17 ms (47.65%), Space: 26.4 MB (55.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
       std::vector<int> prod(nums.size(),1);
-      for (auto& x : prod) {
-        x = 1;
-      }
+      
       int temp = 1;
       for (int i = 0; i < nums.size(); i++) {
         prod[i] = prod[i] * temp;
